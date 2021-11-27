9a9254f3bde4844137d448cc8c77c8614e06bde7
Rustam Shubkin <rustshubkinn@mail.ru>
2021-11-27 15:28:41 +0600

    Add reportWebVitals to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { reportWebVitals } from 'reportWebVitals';
+
 import { App } from './App';
 
 ReactDOM.render(
@@ -9,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
